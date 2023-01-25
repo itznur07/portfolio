fea5c4d595f0573a5f45d3275de9fe7e66d1478d
Md Nuruddin <nuruddinalways99@gmail.com>
2023-01-25 23:10:03 +0600

    Add Route all component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import MySkill from './Components/MySkill';
+import Protfolio from './Components/Protfolio';
 import Home from "./Pages/Home";
 
 const App = () => {
   return (
     <div className=' bg-[#111A28]'>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}/>
+        <Route path="/skill" element={<MySkill />}/>
+        <Route path="/portfolio" element={<Protfolio />}/>
+        <Route path="/contact" element={<Contact />}/>
+      </Routes>
     </div>
   );
 };
