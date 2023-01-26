e8140a1dfdcce60857bda0ee33d0390a4150e188
Md Nuruddin <nuruddinalways99@gmail.com>
2023-01-26 08:09:17 +0600

    Add Resume

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -15,7 +15,7 @@ const Banner = () => {
     <div className='container mx-auto'>
       <div className='md:mx-24 mx-10 md:flex items-center md:my-0 my-10 md:gap-x-16 md:h-screen'>
         {/* div 1 */}
-        <div className="md:flex md:flex-col flex flex-col-reverse md:gap-y-20 gap-y-5 items-center">
+        <div className='md:flex md:flex-col flex flex-col-reverse md:gap-y-20 gap-y-5 items-center'>
           <div className='flex md:flex-col gap-6 text-xl text-[#0eb761] cursor-pointer'>
             <Icon icon={<FaGithub />} link={`https://github.com/itznur07`} />
             <Icon
@@ -47,7 +47,13 @@ const Banner = () => {
             Designer
           </span>
           <div className='flex items-center gap-4 mt-5'>
-            <Button name='Get Resume' style={`bg-[#0eb761]`} />
+            <a
+              href='https://drive.google.com/file/d/1ImNylwGYbKjIjaMq7UlpMFPigWhGldtj/view?usp=sharing'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <Button name='Get Resume' style={`bg-[#0eb761]`} />
+            </a>
             <Button name='About me' style={`border border-[#0eb761]`} />
           </div>
         </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,19 +2,19 @@ import React from "react";
 import Button from "./Button";
 
 const Navbar = () => {
-  return(
-    <div className="container mx-auto">
-      <div className="md:flex md:flex-row md:justify-between items-center flex flex-col md:gap-y-0 gap-y-7 justify-center md:mx-24 mx-10 md:py-6 py-5">
+  return (
+    <div className='container mx-auto'>
+      <div className='md:flex md:flex-row md:justify-between items-center flex flex-col md:gap-y-0 gap-y-7 justify-center md:mx-24 mx-10 md:py-6 py-5'>
         {/* div 1 */}
         <div>
-        <span className='text-xl font-semibold text-[#fff] px-3 py-2 rounded-full bg-[#0eb761]'>
+          <span className='text-xl font-semibold text-[#fff] px-3 py-2 rounded-full bg-[#0eb761]'>
             N
           </span>
         </div>
         {/* div 1 */}
         {/* div 2 */}
         <div>
-        <nav>
+          <nav>
             <ul className='flex items-center md:space-x-16 space-x-10 md:text-md text-md text-[#fff] font-medium cursor-pointer'>
               <li className='hover:text-[#0eb761]'>Home</li>
               <li className='hover:text-[#0eb761]'>About</li>
@@ -26,12 +26,18 @@ const Navbar = () => {
         {/* div 2 */}
         {/* div 3 */}
         <div>
-        <Button name='Resume' style={`border border-[#0eb761]`} />
+          <a
+            href='https://drive.google.com/file/d/1ImNylwGYbKjIjaMq7UlpMFPigWhGldtj/view?usp=sharing'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <Button name='Resume' style={`border border-[#0eb761]`} />
+          </a>
         </div>
         {/* div 3 */}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
