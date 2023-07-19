a4e390601d785ed7fb1031e9d3172b509089f923
Md Nuruddin <nuruddinalways99@gmail.com>
2023-07-19 23:30:44 +0600

    DOne

--- src/Components/ProfileCard/ProfileCard.jsx
+++ src/Components/ProfileCard/ProfileCard.jsx
@@ -5,35 +5,39 @@ import personImg from "../../assets/person.png";
 
 const ProfileCard = () => {
   return (
-    <div className='w-full h-full bg-gradient-to-r from-[#262626] to-[#151515]  rounded-3xl p-10 md:mx-0 mx-5 relative group'>
-      <div className='flex items-center space-x-6'>
-        <div>
-          <img
-            src={personImg}
-            className='rounded-tl-3xl rounded-br-3xl border border-[#8a8a8a]'
-            alt='personmage'
-          />
+    <>
+      <Link to='/about'>
+        <div className='w-full h-full bg-gradient-to-r from-[#262626] to-[#151515]  rounded-3xl p-10 md:mx-0 mx-5 relative group'>
+          <div className='flex items-center space-x-6'>
+            <div>
+              <img
+                src={personImg}
+                className='rounded-tl-3xl rounded-br-3xl border border-[#8a8a8a]'
+                alt='personmage'
+              />
+            </div>
+            <div className='flex flex-col space-y-2'>
+              <p className='text-md uppercase font-medium text-[#8a8a8a]'>
+                A MERN Stack Developer
+              </p>
+              <span className='text-4xl text-white font-semibold'>
+                <p>Nur</p>
+                <p>Uddin.</p>
+              </span>
+              <p className='capitalize text-sm font-medium text-[#8a8a8a]'>
+                A MERN stack developer with a passion for creating dynamic and
+                user-friendly applications
+              </p>
+            </div>
+            <div className='absolute bottom-5 right-5 cursor-pointer opacity-40 group-hover:opacity-100 transition-all ease-linear duration-200 delay-100'>
+              <Link to='/about'>
+                <img src={icon} alt='icon' />
+              </Link>
+            </div>
+          </div>
         </div>
-        <div className='flex flex-col space-y-2'>
-          <p className='text-md uppercase font-medium text-[#8a8a8a]'>
-            A MERN Stack Developer
-          </p>
-          <span className='text-4xl text-white font-semibold'>
-            <p>Nur</p>
-            <p>Uddin.</p>
-          </span>
-          <p className='capitalize text-sm font-medium text-[#8a8a8a]'>
-            A MERN stack developer with a passion for creating dynamic and
-            user-friendly applications
-          </p>
-        </div>
-        <div className='absolute bottom-5 right-5 cursor-pointer opacity-40 group-hover:opacity-100 transition-all ease-linear duration-200 delay-100'>
-          <Link to='/about'>
-            <img src={icon} alt='icon' />
-          </Link>
-        </div>
-      </div>
-    </div>
+      </Link>
+    </>
   );
 };
 
--- src/Components/Showwcase/Showcase.jsx
+++ src/Components/Showwcase/Showcase.jsx
@@ -5,20 +5,26 @@ import myWork from "../../assets/my-works.png";
 
 const Showcase = () => {
   return (
-    <div className='rounded-3xl py-10 px-6 bg-gradient-to-r from-[#262626] to-[#151515] relative group'>
-      <div>
-        <img src={myWork} alt='work-imag' className='p-1' />
-      </div>
-      <div>
-        <p className='text-xs uppercase font-medium text-[#8a8a8a] mb-2'>
-          Showcase
-        </p>
-        <p className='text-2xl font-semibold text-white'>Projects</p>
-      </div>
-      <div className='absolute bottom-5 right-5 cursor-pointer opacity-40 group-hover:opacity-100 transition-all ease-linear duration-200 delay-100'>
-        <Link to="/projects"><img src={icon} alt='icon' /></Link>
-      </div>
-    </div>
+    <>
+      <Link to='/projects'>
+        <div className='rounded-3xl py-10 px-6 bg-gradient-to-r from-[#262626] to-[#151515] relative group'>
+          <div>
+            <img src={myWork} alt='work-imag' className='p-1' />
+          </div>
+          <div>
+            <p className='text-xs uppercase font-medium text-[#8a8a8a] mb-2'>
+              Showcase
+            </p>
+            <p className='text-2xl font-semibold text-white'>Projects</p>
+          </div>
+          <div className='absolute bottom-5 right-5 cursor-pointer opacity-40 group-hover:opacity-100 transition-all ease-linear duration-200 delay-100'>
+            <Link to='/projects'>
+              <img src={icon} alt='icon' />
+            </Link>
+          </div>
+        </div>
+      </Link>
+    </>
   );
 };
 
