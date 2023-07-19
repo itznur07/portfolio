b32c3a826ab00725818debc3cb3573897c20b6ea
Md Nuruddin <nuruddinalways99@gmail.com>
2023-07-19 18:49:53 +0600

    Contact page done

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FaMapMarkerAlt, FaPhone } from "react-icons/fa";
 import { SiMaildotru } from "react-icons/si";
 import SocialProfile from "../../Components/SocialProfile/SocialProfile";
+import icon2 from "../../assets/icon2.png";
 
 const Contact = () => {
   const contactInfo = [
@@ -39,7 +40,7 @@ const Contact = () => {
   return (
     <>
       <div className='max-w-6xl mx-auto my-10'>
-        <div className='grid grid-cols-12'>
+        <div className='grid grid-cols-12 gap-10'>
           <div className='col-span-4'>
             {/* heading */}
             <h1 className='text-2xl font-semibold text-[#f5f5f5] uppercase'>
@@ -70,7 +71,57 @@ const Contact = () => {
             </div>
             {/* social info */}
           </div>
-          <div className='col-span-8'>right</div>
+          <div className='col-span-8'>
+            <div className='rounded-3xl p-10 bg-gradient-to-r from-[#262626] to-[#151515] relative group'>
+              <div className='absolute top-0 right-10'>
+                <img src={icon2} alt='icon' />
+              </div>
+              <div className='text-4xl text-white font-semibold leading-tight'>
+                <p>
+                  Let's work <span className='text-[#19beff]'>together.</span>
+                </p>
+              </div>
+              <form className='mt-8'>
+                <div className='mt-2'>
+                  <input
+                    type='text'
+                    placeholder='Name'
+                    className='w-full p-4 bg-gradient-to-r from-[#2a2a2a] to-[#191919] outline-none rounded-lg text-white'
+                  />
+                </div>
+                <div className='mt-2'>
+                  <input
+                    type='email'
+                    placeholder='Email'
+                    className='w-full p-4 bg-gradient-to-r from-[#2a2a2a] to-[#191919] outline-none rounded-lg text-white'
+                  />
+                </div>
+                <div className='mt-2'>
+                  <input
+                    type='text'
+                    placeholder='Subject'
+                    className='w-full p-4 bg-gradient-to-r from-[#2a2a2a] to-[#191919] outline-none rounded-lg text-white'
+                  />
+                </div>
+                <div className='mt-2'>
+                  <textarea
+                    rows={7}
+                    type='text'
+                    placeholder='Message'
+                    className='w-full p-4 bg-gradient-to-r from-[#2a2a2a] to-[#191919] outline-none rounded-lg text-white'
+                  />
+                </div>
+                <div className='mt-5'>
+                  <button
+                    type='submit'
+                    className='w-full px-5 py-2.5 text-[#fff] hover:text-[#000] bg-[#323232] hover:bg-[#f5f5f5] font-medium rounded-lg transition ease-linear duration-200 delay-75'
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </>
