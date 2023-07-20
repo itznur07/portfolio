2045fb85dd8d4d7d728324123a9ff35ca7a4f448
Md Nuruddin <nuruddinalways99@gmail.com>
2023-07-20 10:15:06 +0600

    Project page responsive

--- src/Pages/Porjects/Protfolio.js
+++ src/Pages/Porjects/Protfolio.js
@@ -40,59 +40,73 @@ const Protfolio = () => {
   ];
 
   return (
-    <div>
+    <div className='max-w-6xl md:mx-auto mx-10'>
       <div className='mt-10'>
         <h1 className='text-7xl font-bold uppercase text-center text-[#f5f5f5]'>
           MY WORK
         </h1>
       </div>
       {projectData.map((project) => (
-        <div className='max-w-6xl mx-auto my-5 rounded-3xl py-10 px-6 bg-gradient-to-r from-[#262626] to-[#151515] relative group shadow shadow-black'>
-          <div className='flex justify-center bg-[#1D1D1D] border border-white border-opacity-5 text-white  rounded-3xl  items-center space-x-4'>
+        <div
+          key={project.id}
+          className='max-w-6xl mx-auto my-5 rounded-3xl py-10 px-6 bg-gradient-to-r from-[#262626] to-[#151515] relative group shadow shadow-black'
+        >
+          {/* Project Image */}
+          <div className='w-full aspect-w-16 aspect-h-9 bg-[#1D1D1D] border border-white border-opacity-5 rounded-3xl overflow-hidden'>
             <img
               src={project.image}
               alt='project-cover'
-              className='w-full h-full object-cover rounded-3xl border border-white border-opacity-40 '
+              className='object-cover w-full h-full'
             />
           </div>
+
+          {/* Project Description and Title */}
           <div className='mt-7'>
             <p className='text-xs uppercase font-medium text-[#8a8a8a] mb-2'>
               {project.description}
             </p>
             <p className='text-2xl font-semibold text-white'>{project.title}</p>
           </div>
-          {/* 2 */}
-          <div className='absolute left-80 cursor-pointer'>
+
+          {/* Project Tags */}
+          <div className='flex flex-wrap mt-4 md:space-x-2 gap-5'>
             {project.tags.map((tag, i) => (
               <span
-                className='mr-5 text-white bg-gradient-to-r from-[#262626] to-[#151515] p-2 rounded-lg px-4 text-xs border border-white border-opacity-30 '
                 key={i}
+                className='text-white bg-gradient-to-r from-[#262626] to-[#151515] px-3 py-1 rounded-lg text-xs border border-white border-opacity-30 '
               >
                 {tag}
               </span>
             ))}
           </div>
-          {/* 3 */}
-          <div className='absolute bottom-10 right-7 cursor-pointer'>
-            <div className='flex items-center space-x-5'>
-              <a href={project.livelink} target="black">
-                <img
-                  src={icon}
-                  alt='icon'
-                  className='opacity-40 group-hover:opacity-100 transition-all ease-linear duration-200 delay-100 '
-                />
-              </a>
-              <span className='text-xl text-white'>
-                <a href={project.serverLink} target='black'>
-                  <FaServer></FaServer>
-                </a>
-              </span>
-              <span className='text-xl text-white'>
-                <a href={project.repoLink} target="black">
-                  <FaGithub></FaGithub>
-                </a>
-              </span>
-            </div>
+
+          {/* Project Links */}
+          <div className='absolute flex items-center bottom-5 right-4 space-x-3'>
+            <a
+              href={project.livelink}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img
+                src={icon}
+                alt='icon'
+                className='w-12 h-12 opacity-40 group-hover:opacity-100 transition-opacity ease-in-out duration-200'
+              />
+            </a>
+            <a
+              href={project.serverLink}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaServer className='text-white text-xl' />
+            </a>
+            <a
+              href={project.repoLink}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaGithub className='text-white text-xl' />
+            </a>
           </div>
         </div>
       ))}
